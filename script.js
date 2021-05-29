55d3a5c2ac1dae4f1e74ccc0491628fcd723b9d9
Danielle <danielle@kendallspace.net>
2021-05-29 16:20:15 -0400

    Simplified the page, Updated CSS

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function googleApps() {
-    var x = document.getElementById("popup");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
-}
-
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
